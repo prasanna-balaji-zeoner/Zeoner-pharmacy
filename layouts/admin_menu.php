23f1c968d0ec4349d4ce8a28445b6f1fcdfa1fcf
Prasanna.balaji <prasannabalaji.zeoner@gmail.com>
2021-02-04 18:18:00 +0530

    Expired is wanted to added

---
+++ layouts/admin_menu.php
@@ -0,0 +1,63 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categories</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage products</a> </li>
+       <li><a href="add_product.php">Add product</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="expiry.php" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Expired Products</span>
+      </a>
+      
+  </li>
+  <li>
+  
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Meadias</span>
+    </a>
+  </li>
+  <li>
+    <a href="add_sale.php" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Add Sales</span>
+      </a>
+     
+  </li>
+  <li>
+    <a href="sales.php" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Sales Report</span>
+      </a>
+      
+  </li>
+  
+</ul>
