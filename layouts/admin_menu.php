bb0bac7b0d9df6e704609c4b721da358b3057553
Prasanna.balaji <prasannabalaji.zeoner@gmail.com>
2021-02-06 13:30:15 +0530

    Layout and Stock add

--- layouts/admin_menu.php
+++ layouts/admin_menu.php
@@ -6,16 +6,6 @@
     </a>
   </li>
   <li>
-    <a href="#" class="submenu-toggle">
-      <i class="glyphicon glyphicon-user"></i>
-      <span>User Management</span>
-    </a>
-    <ul class="nav submenu">
-      <li><a href="group.php">Manage Groups</a> </li>
-      <li><a href="users.php">Manage Users</a> </li>
-   </ul>
-  </li>
-  <li>
     <a href="categorie.php" >
       <i class="glyphicon glyphicon-indent-left"></i>
       <span>Categories</span>
@@ -31,33 +21,51 @@
        <li><a href="add_product.php">Add product</a> </li>
    </ul>
   </li>
-  <li>
+  <!-- <li>
     <a href="expiry.php" class="submenu-toggle">
       <i class="glyphicon glyphicon-signal"></i>
        <span>Expired Products</span>
       </a>
-      
-  </li>
-  <li>
-  
+  </li> -->
+  <!-- <li>
     <a href="media.php" >
       <i class="glyphicon glyphicon-picture"></i>
       <span>Meadias</span>
     </a>
-  </li>
+  </li> -->
   <li>
     <a href="add_sale.php" class="submenu-toggle">
       <i class="glyphicon glyphicon-th-list"></i>
        <span>Add Sales</span>
       </a>
-     
   </li>
-  <li>
+  <!-- <li>
     <a href="sales.php" class="submenu-toggle">
       <i class="glyphicon glyphicon-signal"></i>
        <span>Sales Report</span>
       </a>
       
+  </li> -->
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Report</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="sales.php">Sales Report</a> </li>
+      <li><a href="expiry.php">Expired Report</a> </li>
+      <li><a href="stock.php">Stock Report</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
   </li>
   
 </ul>
---
+++ stock.php
@@ -0,0 +1,72 @@
+<?php
+  $page_title = 'All Product';
+  require_once('includes/load.php');
+  // Checkin What level user has permission to view this page
+   page_require_level(2);
+  $products = join_product_table();
+?>
+<?php include_once('layouts/header.php'); ?>
+  <div class="row">
+     <div class="col-md-12">
+       <?php echo display_msg($msg); ?>
+     </div>
+    <div class="col-md-12">
+      <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+         <div class="pull-right">
+           <a href="add_product.php" class="btn btn-primary">Add New</a>
+         </div>
+        </div>
+        <div class="panel-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th class="text-center" style="width: 50px;">#</th>
+                <th> Product Name </th>
+                <th class="text-center" style="width: 10%;"> Categorie </th>
+                <th class="text-center" style="width: 10%;"> Instock </th>
+                <th class="text-center" style="width: 10%;"> Buying Price </th>
+                <th class="text-center" style="width: 10%;"> Saleing Price </th>
+                <th class="text-center" style="width: 10%;"> Product Added </th>
+                <th class="text-center" style="width: 10%;"> Expiry Date </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($products as $product):?>
+                <?php if ($product['quantity'] < 500) {
+                     ?>
+
+              <tr>
+                <td class="text-center"><?php echo count_id();?></td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td class="text-center" > <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center" style="color:red;"> <?php echo remove_junk($product['quantity']); ?><br><div class="pull-right">
+           <a href="add_product.php" class="btn btn-primary">Add Stock</a>
+         </div></td>
+                <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['sale_price']); ?></td>
+                <td class="text-center"> <?php echo read_date($product['date']); ?></td>
+                <td class="text-center"> <?php echo $product['exp_date']; ?></td>
+              </tr>
+              <?php } 
+              else {?>
+               <tr>
+                <td class="text-center"><?php echo count_id();?></td>
+                <td> <?php echo remove_junk($product['name']); ?></td>
+                <td class="text-center" > <?php echo remove_junk($product['categorie']); ?></td>
+                <td class="text-center" > <?php echo remove_junk($product['quantity']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['buy_price']); ?></td>
+                <td class="text-center"> <?php echo remove_junk($product['sale_price']); ?></td>
+                <td class="text-center"> <?php echo read_date($product['date']); ?></td>
+                <td class="text-center"> <?php echo $product['exp_date']; ?></td>
+              </tr>
+              <?php  
+              }?>
+             <?php endforeach; ?>
+            </tbody>
+          </tabel>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include_once('layouts/footer.php'); ?>
